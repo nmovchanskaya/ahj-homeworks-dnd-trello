2617bb104457e0829dbcb528e2340d8c91f72b99
nika.movchanskaya <nika.movchanskaya@gmail.com>
2023-09-12 02:51:04 +0200

    initial commit

--- src/desk.js
+++ src/desk.js
@@ -20,51 +20,74 @@ export default class Desk {
     this.taskWidgetDone.bindToDOM();
   }
 
+  //return tasks inside the container
+  getTasks(container, type) {
+    const taskElems = Array.from(container.querySelectorAll('.task'));
+    const tasks = [];
+    taskElems.forEach((item) => {
+      const txt = item.innerText.trim();
+      tasks.push(new Task(txt.substring(0, txt.length - 2), type, Number(item.dataset.id)));
+    });
+
+    return tasks;
+  }
+
+  //update widget with the tasks
+  updateWidgets(tasks, type) {
+    if (type === 'todo') {
+      this.taskWidgetTodo.update(tasks);
+    } else if (type === 'inprogress') {
+      this.taskWidgetInprogress.update(tasks);
+    } else if (type === 'done') {
+      this.taskWidgetDone.update(tasks);
+    }
+  }
+
   onMouseUp(e) {
     const mouseUpItem = e.target;
 
     if (mouseUpItem.className === 'task') {
       const taskContainer = mouseUpItem.parentElement;
 
-      console.log(mouseUpItem);
-      console.log(taskContainer);
       taskContainer.insertBefore(this.actualElement, mouseUpItem);
 
       // update lists of tasks in memory
       const container = document.querySelector(`.task__container_${this.actualType}`);
-      const taskElems = Array.from(container.querySelectorAll('.task'));
-      const tasks = [];
-      taskElems.forEach((item) => {
-        const txt = item.innerText.trim();
-        tasks.push(new Task(txt.substring(0, txt.length - 2), this.actualType, Number(item.dataset.id)));
-      });
 
-      if (this.actualType === 'todo') {
-        this.taskWidgetTodo.update(tasks);
-      } else if (this.actualType === 'inprogress') {
-        this.taskWidgetInprogress.update(tasks);
-      } else if (this.actualType === 'done') {
-        this.taskWidgetDone.update(tasks);
-      }
+      const tasks = this.getTasks(container, this.actualType);
+      this.updateWidgets(tasks, this.actualType);
 
       // if we move task to another column - update it also
       if (taskContainer.dataset.type !== container.dataset.type) {
-        const taskElemsTo = Array.from(taskContainer.querySelectorAll('.task'));
-        const tasksTo = [];
-        taskElemsTo.forEach((item) => {
-          tasksTo.push(new Task(item.textContent.trim(), taskContainer.dataset.type, item.dataset.id));
-        });
 
-        if (taskContainer.dataset.type === 'todo') {
-          this.taskWidgetTodo.update(tasksTo);
-        } else if (taskContainer.dataset.type === 'inprogress') {
-          this.taskWidgetInprogress.update(tasksTo);
-        } else if (taskContainer.dataset.type === 'done') {
-          this.taskWidgetDone.update(tasksTo);
-        }
+        const tasksTo = this.getTasks(taskContainer, taskContainer.dataset.type);
+        this.updateWidgets(tasksTo, taskContainer.dataset.type);
       }
 
       this.taskWithSpace.style.borderTop = 0;
+
+      this.actualElement.classList.remove('dragged');
+      this.actualElement = undefined;
+
+      document.documentElement.removeEventListener('mouseup', this.onMouseUp);
+      document.documentElement.removeEventListener('mouseover', this.onMouseOver);
+    }
+
+    //if we don't have any elements in target column - create first
+    const taskQty = e.target.querySelectorAll('.task').length;
+    if (e.target.classList.contains('column') && taskQty === 0) {
+
+      // find task container inside target column
+      const taskContainer = e.target.querySelector('.task__container');
+      taskContainer.insertBefore(this.actualElement, null);
+
+      const container = document.querySelector(`.task__container_${this.actualType}`);
+
+      const tasks = this.getTasks(container, this.actualType);
+      this.updateWidgets(tasks, this.actualType);
+
+      const tasksTo = this.getTasks(taskContainer, taskContainer.dataset.type);
+      this.updateWidgets(tasksTo, taskContainer.dataset.type);
 
       this.actualElement.classList.remove('dragged');
       this.actualElement = undefined;
